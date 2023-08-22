1d6311f11acecd1aeff299edd67e4ce75c2ad0a4
Chatkamon <chatkamon_bee@hotmail.com>
2023-08-22 12:17:01 +1000

    first commit

--- backend/src/routes/greeting.ts
+++ backend/src/routes/greeting.ts
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/', (_req, res) => {
   const response: Greeting = {
-    message: 'Hello, world!'
+    message: "I looked back to my engineering roots, something I always had a keen interest in growing up."
   }
 
   res.json(response)
