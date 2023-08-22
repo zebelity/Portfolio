8e34c2302e74de73796ca81be289300b0052b1cc
Chatkamon <chatkamon_bee@hotmail.com>
2023-08-22 17:58:13 +1000

    Fix build issues

--- src/api.ts
+++
@@ -1,9 +0,0 @@
-import type { Greeting } from '@shared/types'
-import env from './env'
-
-export async function getGreeting () {
-  const response = await fetch(`${env.serverUrl}/greeting`)
-  const result = await response.json() as Greeting
-
-  return result
-}
